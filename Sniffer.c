31c3d6142cba372ec9a7603da051682f406d1829
Sagit Malka <118733667+SagitMalka@users.noreply.github.com>
2023-01-23 22:24:39 +0200

    print all

--- Sniffer.c
+++ Sniffer.c
@@ -10,88 +10,44 @@
 #define SNAPLEN 1518
 
 
-/**TO DO
- * print packet info using packet handler
- * all fields required
- * */
-//void print_pcap_packet(const u_char *, int);
 
-void print_ip_packet(const u_char *, int);
 void PrintData (const u_char * data , int Size);
-void print_tcp_packet(const u_char *buffer, int size);
-//void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+void print_the_packet(const u_char *buffer, int size);
+
 
 FILE *logfile;
 struct sockaddr_in source, dest;
-//struct pcap_pkthdr *header;
+
 
 struct iphdr *iphdr;
 struct ethhdr *ethhdr;
 
+
+struct myHeader
+{
+    u_int32_t timestamp;
+    u_int16_t total_lenght;
+    u_char saved : 3;
+    u_char cache_flag : 1;
+    u_char steps_flag : 1;
+    u_char type_flag : 1;
+    u_int16_t status_code : 10;
+    u_int16_t cache_control;
+    u_int16_t padding;
+};
 void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
 
     int size = header->len;
+        print_the_packet(packet, size);
 
-    struct iphdr *iphdr1 = (struct iphdr*)(packet + sizeof(struct ethhdr));
-    if(iphdr1->protocol == 0){
-
-    }
-    print_tcp_packet(packet, size);
-
-    struct ether_header *etherHeader;
-    etherHeader = (struct ether_header *) packet;
-    if (ntohs(etherHeader->ether_type) != ETHERTYPE_IP) {
-        return;
-    }
-    const u_char *ip_header;
-    const u_char *tcp_header;
-    const u_char *payload;
-
-    int ethernet_header_length = 14;
-    int ip_header_length;
-    int tcp_header_length;
-    int payload_length;
-
-    ip_header = packet + ethernet_header_length;
-    ip_header_length = ((*ip_header) & 0x0F);
-    ip_header_length = ip_header_length * 4;
-
-    u_char protocol = *(ip_header + 9);
-    if (protocol != IPPROTO_TCP) {
-        return;
-    }
-    tcp_header = packet + ethernet_header_length + ip_header_length;
-    tcp_header_length = ((*tcp_header + 12) & 0xF0) >> 4;
-    tcp_header_length = tcp_header_length * 4;
-
-    int total_headers_size = ethernet_header_length + ip_header_length + tcp_header_length;
-
-    payload_length = header->caplen - total_headers_size;
-    payload = packet + total_headers_size;
-
-
-
-    return;
 }
 
 
-//void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
-//{
-//    int i;
-//
-//    fprintf(logfile,"Packet of length %d:\n", header->len);
-//    for (i = 0; i < header->len; i++) {
-//        fprintf(logfile, "%02x ", packet[i]);
-//    }
-//    fprintf(logfile, "\n");
-//}
 
 int main() {
     printf("sup\n");
 
     /**SETTING THE DEVICE**/
-//    char *dev = NULL;
-
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *handle;
 
@@ -106,13 +62,12 @@ int main() {
     /**OPENING THE DEVICE FOR SNIFFING**/
     handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
-//        printf("error: \"%s\"\n", errbuf);
         fprintf(stderr, "Can't open enp0s3: %s\n", errbuf);
         return -1;
     }
 
 
-    logfile = fopen("log.txt", "w");
+    logfile = fopen("209294768_206477788.txt", "w");
     if (logfile == NULL) {
         fprintf(stderr, "Can't open file: %s\n", errbuf);
         return -1;
@@ -139,8 +94,10 @@ int main() {
     return (0);
 }
 
-void print_ip_packet(const u_char *buffer, int size) {
+
+void print_the_packet(const u_char *buffer, int size) {
     unsigned short ip_header_len;
+
     struct iphdr *iphdr1 = (struct iphdr *) (buffer + sizeof(struct ethhdr));
     ip_header_len = iphdr1->ihl * 4;
 
@@ -150,36 +107,28 @@ void print_ip_packet(const u_char *buffer, int size) {
     memset(&dest, 0, sizeof(dest));
     dest.sin_addr.s_addr = iphdr1->daddr;
 
-    fprintf(logfile, " source ip: %s", inet_ntoa(source.sin_addr));
-    fprintf(logfile, ", dest ip: %s\n", inet_ntoa(dest.sin_addr));
-}
 
-void print_tcp_packet(const u_char *buffer, int size) {
-    unsigned short ip_header_len;
-
-    struct iphdr *iphdr1 = (struct iphdr *) (buffer + sizeof(struct ethhdr));
-    ip_header_len = iphdr1->ihl * 4;
 
     struct tcphdr *tcphdr = (struct tcphdr *) (buffer + ip_header_len + sizeof(struct ethhdr));
     int header_size = sizeof(struct ethhdr) + ip_header_len + tcphdr->doff * 4;
 
     fprintf(logfile, "\ncaptured packet: \n");
 
-    print_ip_packet(buffer, size);
-
+    fprintf(logfile, " source ip: %s", inet_ntoa(source.sin_addr));
+    fprintf(logfile, ", dest ip: %s\n", inet_ntoa(dest.sin_addr));
     fprintf(logfile, " source port: %u", ntohs(tcphdr->source));
-    fprintf(logfile, " dest port: %u\n", ntohs(tcphdr->dest));
-    fprintf(logfile, " timestamp: ", ntohl(tcphdr->th_seq));
-    
+    fprintf(logfile, " dest port: %u", ntohs(tcphdr->dest));
+   struct myHeader *my_hdr = (struct myHeader *) (struct msghdr *) (buffer + header_size + ip_header_len +
+                                                                    sizeof(struct ethhdr));
+    fprintf(logfile, " timestamp: %u", my_hdr->timestamp);
+    fprintf(logfile, " total_length: %u", my_hdr->total_lenght);
+    fprintf(logfile, " cache_flag: %hu", (unsigned int )(my_hdr->cache_flag));
+    fprintf(logfile, " steps_flag: %hu", (unsigned int )(my_hdr->steps_flag));
+    fprintf(logfile, " type_flag: %hu", (unsigned int )(my_hdr->type_flag));
+    fprintf(logfile, " status_code: %hu", (unsigned int )(my_hdr->status_code));
+    fprintf(logfile, " cache_control: %hu", (unsigned int )(my_hdr->cache_control));
 
-
-    fprintf(logfile , "IP Header\n");
-    PrintData(buffer,ip_header_len);
-
-    fprintf(logfile , "TCP Header\n");
-    PrintData(buffer+ip_header_len,tcphdr->doff*4);
-
-    fprintf(logfile , "Data Payload\n");
+    fprintf(logfile , " Data Payload\n");
     PrintData(buffer + header_size , size - header_size );
 }
 void PrintData (const u_char * data , int Size)
